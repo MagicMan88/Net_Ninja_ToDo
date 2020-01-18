a0d9dcc86c21657c935247fd4464b2f9ab84feca
Stephen Green <stephengreen1028@gmail.com>
2020-01-17 21:15:20 -0700

    Working on modularization aka connecting js files to one another. Started by creating a counter variable and passing in an array and using the arr.length

---
+++ assets/count.js
@@ -0,0 +1,10 @@
+// Working on modularization aka connecting js files to one another
+// ================================================================
+
+// 
+var counter = function(arr){
+    return 'There are ' + arr.length + ' elements in this array';
+};
+
+// Console.log the calling of the function to see it in the terminal
+console.log(counter(['Steve', 'Kelly', 'Humphrey']));
