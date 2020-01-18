11844b2fd1de5e7c786be3dd011a6e1c4fb1150c
Stephen Green <stephengreen1028@gmail.com>
2020-01-17 20:56:46 -0700

    User clearInterval with setInterval to 'break' the infinite loop.

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,11 @@
 // Time variable
 var time = 0;
 
-// setInterval function in node that console.log's a message after five seconds have passed.
-setInterval(function(){
+// setInterval function in node that console.log's a message after five seconds have passed. Add a condition that clears the interval after 5 seconds.
+var timer = setInterval(function(){
     time += 2;
     console.log(time + ' seconds have passed');
+    if (time > 5){
+        clearInterval(timer);
+    }
 }, 2000);
