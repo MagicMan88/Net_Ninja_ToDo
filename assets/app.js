3b09683b40d09f338aca738f67282d1b07f118c4
Stephen Green <stephengreen1028@gmail.com>
2020-01-17 21:08:31 -0700

    working on callback functions.. passing one function into another through variables and callbacks. Practicing advanced js and node.js

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,12 @@
-// Normal function statement
-function sayHi (){
-    console.log('Hi');
+// Call function, taking a function as a parameter. I believe this is a callback.
+function callFunction(fun){
+    fun();
 }
-
-// Call the function
-sayHi();
 
 // Function expression. Assigned a variable named 'sayBye' to an anonymous function and then called it by typing out the variable
 var sayBye = function(){
     console.log('Bye');
 };
 
-sayBye();
+// Call the callFunction and pass in the sayBye function expression
+callFunction(sayBye);
