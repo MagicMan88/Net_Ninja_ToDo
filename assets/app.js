3057d2442247d6c46901b4ca0c3335f73ac65d2d
Stephen Green <stephengreen1028@gmail.com>
2020-01-17 21:05:14 -0700

    Writing a normal function and then a function expression to learn the difference. Practicing node.js

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,14 @@
-// Global way of seeing what directory you're in
-console.log(__dirname);
-// Global way of seeing what file you're in
-console.log(__filename);
+// Normal function statement
+function sayHi (){
+    console.log('Hi');
+}
+
+// Call the function
+sayHi();
+
+// Function expression. Assigned a variable named 'sayBye' to an anonymous function and then called it by typing out the variable
+var sayBye = function(){
+    console.log('Bye');
+};
+
+sayBye();
