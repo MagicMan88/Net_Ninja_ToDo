5bd969d9eddb7112f836c79e567273ccb3d98717
Stephen Green <stephengreen1028@gmail.com>
2020-01-17 20:52:10 -0700

    Used setInterval to console.log a message after every two second interval and added it to a time variable that was initially set to 0

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,8 @@
-// setTimout function in node that console.log's a message after five seconds have passed.
-setTimeout(function(){
-    console.log('5 seconds have passed.');
-}, 5000);
+// Time variable
+var time = 0;
+
+// setInterval function in node that console.log's a message after five seconds have passed.
+setInterval(function(){
+    time += 2;
+    console.log(time + ' seconds have passed');
+}, 2000);
