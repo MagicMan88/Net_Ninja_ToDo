1e88c4c652b9d689ffe9103059792287ee07d30b
Stephen Green <stephengreen1028@gmail.com>
2020-01-17 21:44:49 -0700

    Cutting out some unneccessary code by adding module.exports.'whatever' to each function. Practicing node.js

--- assets/stuff.js
+++ assets/stuff.js
@@ -1,21 +1,21 @@
 // Working on modularization aka connecting js files to one another
 // ================================================================
 
-// Counter function
-var counter = function (arr) {
+// Counter function -- Cutting out the 'middle-man' and 'shortening' the code.
+module.exports.counter = function (arr) {
     return 'There are ' + arr.length + ' elements in this array';
 };
 
 // Adder function using template strings 'eliminates need for a lot of concatenation'
-var adder = function (a, b) {
+module.exports.adder = function (a, b) {
     return `The sum of the 2 numbers is ${a+b}`;
 };
 
 // Pi function
-var pi = 3.142;
+module.exports.pi = 3.142;
 
 // Explicitly state which parts of this module we want other files to have access to that require this module.
 // module.exports is an empty object, add more into the object by using the syntax below.
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
+// module.exports.counter = counter;
+//  module.exports.adder = adder;
+//  module.exports.pi = pi;
