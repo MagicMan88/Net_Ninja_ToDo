45badb8a0ba00b7af9ef66267863ba9ee9ddd3ca
Stephen Green <stephengreen1028@gmail.com>
2020-01-24 21:51:03 -0700

    using fs module to learn about asynchronous functions

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,13 @@
 // Require in necessary dependencies in this case we're using fs 'reading and writing files'
 var fs = require('fs');
 
-// Read a file using fs synchronous
-var readMe = fs.readFileSync('readMe.txt', 'utf8');
+// Read a file using fs asynchronously
+var fs = require('fs');
+ fs.readFile('readMe.txt', 'utf8', function (err, data) {
+  fs.writeFile('writeMe.txt', data, function(err, result) {
+     if(err) console.log('error', err);
+   });
+ });
 
-
-// Writing a new file using fs synchronous
-fs.writeFileSync('writeMe.txt', readMe);
+// // Writing a new file using fs asynchronously
+// fs.writeFileSync('writeMe.txt', readMe);
