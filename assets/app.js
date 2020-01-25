78f0d946d76c04124f16b8882b8154f90e48c322
Stephen Green <stephengreen1028@gmail.com>
2020-01-24 21:21:15 -0700

    Learning about event module and event emitters. Practicing node.js

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,12 @@
 // Use the require function which is on the global object in node.js so we can use it wherever we are.
 // ==================================================================================================
+// Using the events module
+var events = require('events');
 
-// Using require to 'attach' count.js to app.js, we do this by setting require equal to a variable that's equal to whatever's returned to us.
-// The variable 'stuff' is now the module.exports that's returned to this file from the stuff.js file.
-var stuff = require('./stuff')
+var myEmitter = new events.EventEmitter();
 
-// Console.log the calling of the function to see it in the terminal
-console.log(stuff.counter(['Steve', 'Kelly', 'Humphrey']));
-console.log(stuff.adder(5, 6));
-console.log(stuff.adder(stuff.pi, 5));
-console.log(stuff.adder(18, 6));
+myEmitter.on('someEvent', function(mssg){
+    console.log(mssg);
+});
+
+myEmitter.emit('someEvent', 'The event was emitted.');
